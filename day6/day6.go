1bcc7f09f6e185558837d204532f80d28eeac628
Sergey Strelnikov <eloimann@gmail.com>
2025-02-06 20:56:52 +0300

    day6 part 1 answer ok

--- day6/day6.go
+++ day6/day6.go
@@ -60,8 +60,81 @@ func readInput(filename string) ([][]byte, int, int) {
 	return result, res[0], res[1]
 }
 
-func Execute() {
-	input, x, y := readInput("input.txt")
+func makeBoundsChecker(szx, szy int) func(x, y int) bool {
+	return func(x, y int) bool {
+		return x < 0 || x >= szx || y < 0 || y >= szy
+	}
+}
 
-	fmt.Printf("%dx%d %d %d", len(input[0]), len(input), x, y)
+const (
+	STATE_UP = iota
+	STATE_DOWN
+	STATE_RIGHT
+	STATE_LEFT
+)
+
+func outputPole(pole [][]byte) {
+	file, _ := os.Create("output.txt")
+	writer := bufio.NewWriter(file)
+	for y := 0; y < len(pole); y++ {
+		writer.Write(pole[y])
+		writer.WriteByte('\n')
+	}
+	writer.Flush()
+	defer file.Close()
+}
+
+func Execute() {
+	input, xc, yc := readInput("input.txt")
+
+	boundsChecker := makeBoundsChecker(len(input[0]), len(input))
+
+	fmt.Printf("%dx%d %d %d\n", len(input[0]), len(input), xc, yc)
+	input[yc][xc] = '.'
+
+	x, y := xc, yc
+	state := STATE_UP
+	total := 0
+	for !boundsChecker(x, y) {
+		ch := &input[y][x]
+		if *ch == '#' {
+			switch state {
+			case STATE_UP:
+				state = STATE_RIGHT
+				y++
+			case STATE_RIGHT:
+				state = STATE_DOWN
+				x--
+			case STATE_DOWN:
+				state = STATE_LEFT
+				y--
+			case STATE_LEFT:
+				x++
+				state = STATE_UP
+			default:
+				log.Fatal("Wrong flow")
+				os.Exit(1)
+			}
+		}
+		if *ch == '.' {
+			*ch = 'X'
+			total++
+		}
+		switch state {
+		case STATE_UP:
+			y--
+		case STATE_RIGHT:
+			x++
+		case STATE_DOWN:
+			y++
+		case STATE_LEFT:
+			x--
+		default:
+			log.Fatal("Wrong flow")
+			os.Exit(1)
+		}
+	}
+
+	outputPole(input)
+	fmt.Printf("total=%d\n", total)
 }
